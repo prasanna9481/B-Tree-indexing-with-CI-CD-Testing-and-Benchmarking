8c75399f5b3cc6b73511e836a487ab2f5a1e226f
prasanna9481 <prasannavbhat9@gmail.com>
2025-05-16 23:21:05 +0200

    fixed the bug testing pipeline

--- run-all-tests.sh
+++ run-all-tests.sh
@@ -1,11 +1,11 @@
 
-python driver.py test-data/small.txt python sut.py
+python3 driver.py test-data/small.txt python3 sut.py
 
-python driver.py test-data/lookup.txt python sut.py
-python driver.py test-data/lookup_big.txt python sut.py
+python3 driver.py test-data/lookup.txt python3 sut.py
+python3 driver.py test-data/lookup_big.txt python3 sut.py
 
-python driver.py test-data/remove.txt python sut.py
-python driver.py test-data/remove_big.txt python sut.py
+python3 driver.py test-data/remove.txt python3 sut.py
+python3 driver.py test-data/remove_big.txt python3 sut.py
 
-python driver.py test-data/lowerbound.txt python sut.py
-python driver.py test-data/lowerbound_big.txt python sut.py
+python3 driver.py test-data/lowerbound.txt python3 sut.py
+python3 driver.py test-data/lowerbound_big.txt python3 sut.py
